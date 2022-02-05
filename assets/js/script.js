2fe3ef1275688fbf50c8ca349859c78ff558eb88
chloë js <chloejs.horta@gmail.com>
2022-02-05 14:08:57 +0000

    set up structure of game

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+function calculateAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementIncorrectAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+function displayDivionQuestion(){
+
+}
